d9d446782b99005d87e49deb43935fa2905ca5fe
imman-thomas <immanthomas@gmail.com>
2022-02-05 17:42:54 +0800

    Initial Commit

---
+++ welcomeBash.sh
@@ -0,0 +1,13 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+PURPLE='\x1B[01;95m'
+#echo -e "I ${RED}love${NC} Stack Overflow"
+echo -e "${PURPLE}        OO O o o o... ______________________ _________________ "
+echo -e "${PURPLE}  O     ____          |                    | |               | "
+echo -e "${PURPLE} ][_n_i_| (   ooo___  |                    | |               | "
+echo -e "${PURPLE}(__________|_[______]_|____________________|_|_______________| "
+echo -e "${PURPLE}  0--0--0      0  0      0       0     0        0        0     "
+
+echo -e "${RED}And we know that all things work together for good to them that love God, 
+to them who are the called according to his purpose
+							Romans 28 : 8"
